4b6a00367828cb4b47eddb76ffeca2a22dc33cde
mpilopillz <mpilo.dlamini@yahoo.com>
2024-06-21 22:14:27 +0200

    creating data service

---
+++ modular-playground/src/app/shared/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor() {}
+
+  getDetails(): Promise<string> {
+    const resultPromise: Promise<string> = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Data');
+      }, 1500);
+    });
+    return resultPromise;
+  }
+}
