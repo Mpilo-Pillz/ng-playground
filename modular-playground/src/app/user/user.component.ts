74bfbd60e668fd15d7611b2c89cff4add9ae6912
mpilopillz <mpilo.dlamini@yahoo.com>
2024-06-22 22:14:01 +0200

    using data service

--- modular-playground/src/app/user/user.component.ts
+++ modular-playground/src/app/user/user.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   isLoggedIn = false;
+  data: string | undefined;
   user: {
     name: string;
     password: string;
   };
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private dataService: DataService
+  ) {
     this.user = this.userService.user;
+  }
+
+  ngOnInit(): void {
+    this.dataService.getDetails().then((data: string) => (this.data = data));
   }
 }
